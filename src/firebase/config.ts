7ce4340aeca972eaa13eb8c37055bb16bbd5dad9
Arbind Kumar Yadav <arbind.connects@gmail.com>
2026-01-05 15:15:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8872425499-c9589",
+  "appId": "1:243366151106:web:3b8bf12dd08ca633e38c5b",
+  "apiKey": "AIzaSyDV0xvUWMmdiSmHZWbTxawKNyOcgTcFHRw",
+  "authDomain": "studio-8872425499-c9589.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "243366151106"
+};
